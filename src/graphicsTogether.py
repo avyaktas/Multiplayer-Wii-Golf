a290985a10ca342c50d5d6dbb5d5783e8925b78f
andrewChurchey <achurche@andrew.cmu.edu>
2025-04-21 16:45:21 -0400

    small change

--- src/graphicsTogether.py
+++ src/graphicsTogether.py
@@ -291,6 +291,7 @@ def onStep(app):
         # Check if ball has landed
         if app.ballZ <= 0 and app.ballVelocityZ < 0:
             app.ballZ = 0
+            app.shadowY = app.ballY
             app.ballInMotion = False
             app.ballVelocityZ = 0 
             app.aimAngle = math.atan2(app.targetY - app.ballY,
