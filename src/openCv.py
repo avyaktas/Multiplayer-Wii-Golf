61ba8530aa1517ac7c374f912dfe1337b49ac6d1
NLM888 <76796269+NLM888@users.noreply.github.com>
2025-04-23 19:26:28 -0400

    here

--- src/openCv.py
+++
@@ -1,8 +0,0 @@
-import cv2            # OpenCV’s Python bindings
-import numpy as np    # For array manipulation
-from tkinker import Tk  # For GUI
-from tkinker.filedialog import askopenfilename  # For file dialog
-from PIL import Image, ImageTk  # For image handling
-image = cv2.imread('image.jpg')  # Load an image
-if image == None:
-    raise Exception("Image not found!")
